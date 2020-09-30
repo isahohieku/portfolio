f43f5e083db92ec6037d6574fa15f424453a6aee
Isah Ohieku <isahohieku@gmail.com>
2020-09-30 17:23:32 +0100

    Developed footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainJumbotronComponent } from './main-jumbotron/main-jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainJumbotronComponent
+    MainJumbotronComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

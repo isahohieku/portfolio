ca273bc2cb5598589b226b07f56f5e4e75a98a79
Isah Ohieku <isahohieku@gmail.com>
2023-12-31 12:07:41 +0100

    Add animations

--- src/app/components/landing/about-section/about-section.component.ts
+++ src/app/components/landing/about-section/about-section.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { flyIn } from 'src/app/shared/animations/fly-in';
 
 @Component({
   selector: 'app-about-section',
   templateUrl: './about-section.component.html',
-  styleUrls: ['./about-section.component.scss']
+  styleUrls: ['./about-section.component.scss'],
+  animations: [
+    flyIn
+  ]
 })
 export class AboutSectionComponent implements OnInit {
+  isInView = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  inView() {
+    this.isInView = true;
   }
 
 }
